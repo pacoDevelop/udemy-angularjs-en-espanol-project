245b93ce196b3d15f6d77016e7cbdc91da7e5ac7
pacoDevelop <franciscohernandezsantos3@gmail.com>
2022-11-18 12:40:07 +0100

    38. Insertar en base de datos

--- js/alumnoCtrl.js
+++ js/alumnoCtrl.js
@@ -1,27 +1,51 @@
-app.controller('alumnoCtrl', ['$scope','$routeParams','$http', function($scope,$routeParams,$http){
+app.controller("alumnoCtrl", [
+  "$scope",
+  "$routeParams",
+  "$http",
+  function ($scope, $routeParams, $http) {
+    $scope.setActive("mAlumnos");
+    var codigo = $routeParams.codigo;
+    $scope.actualizado = false;
+    $scope.creando = false;
 
-	$scope.setActive("mAlumnos");
-	var codigo=$routeParams.codigo;
-	$scope.actualizado=false;
+    $scope.alumno = {};
 
-	$scope.alumno={};
-	
-	$http.get("php/servicios/alumnos.getAlumno.php?c="+codigo).then(res=>{
-		
-		if(!res||!res.data||res.data.err!==undefined){
-			window.location="#!/alumnos"
-			return;
-		}
-		$scope.alumno=res.data});
-		
-	$scope.guardarAlumno=()=>{
-		$http.post("php/servicios/alumnos.guardar.php",$scope.alumno).then(res=>{
-			$scope.actualizado=res&&res.data&&res.data.err===false;
-			setTimeout(()=>{
-				$scope.actualizado=false;
-				$scope.$apply();
-			},3500);
-		})
-	}
-			
-}]);
+    if (codigo == "nuevo") {
+      $scope.creando = true;
+    } else {
+      $http
+        .get("php/servicios/alumnos.getALumno.php?c=" + codigo)
+        .then((res) => {
+          if (!res || !res.data || res.data.err !== undefined) {
+            window.location = "#!/alumnos";
+            return;
+          }
+          $scope.alumno = res.data;
+        });
+    }
+
+    $scope.guardarAlumno = () => {
+      if ($scope.creando) {
+        $http
+          .post("php/servicios/alumnos.crear.php", $scope.alumno)
+          .then((res) => {
+            $scope.actualizado = res && res.data && res.data.err === false;
+            setTimeout(() => {
+              $scope.actualizado = false;
+              $scope.$apply();
+            }, 3500);
+          });
+      } else {
+        $http
+          .post("php/servicios/alumnos.guardar.php", $scope.alumno)
+          .then((res) => {
+            $scope.actualizado = res && res.data && res.data.err === false;
+            setTimeout(() => {
+              $scope.actualizado = false;
+              $scope.$apply();
+            }, 3500);
+          });
+      }
+    };
+  },
+]);
